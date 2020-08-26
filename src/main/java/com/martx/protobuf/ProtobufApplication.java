471c39e9351f9d7930851397f478087225b37c71
liubin <liubin>
2020-08-26 11:51:04 +0800

    Initial commit

---
+++ src/main/java/com/martx/protobuf/ProtobufApplication.java
@@ -0,0 +1,13 @@
+package com.martx.protobuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtobufApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtobufApplication.class, args);
+    }
+
+}
